81918c4eb5dd06ca27c6d811c5f019c22b925ee2
egaraev@gmail.com <Kax0vka44609>
2019-01-17 21:12:54 +0100

    start.sh

--- start.sh
+++ start.sh
@@ -3,27 +3,22 @@
 
 while true
 do
+
+
 SERVICE0='buy.py'
 
-if ps ax | grep -v grep | grep $SERVICE0 > /dev/null
-then
-    echo "$SERVICE0 service running "
-else
-    echo there is no such "$SERVICE0 service, starting"
-    python /usr/local/bin/buy.py
-fi
+ps -ef | grep $SERVICE0 | grep -v grep
+[ $?  -eq "0" ] && echo "$SERVICE0 process is running" || echo "$SERVICE0 process is not running, starting"; python /usr/local/bin/buy.py
+
+
 
 
 
 SERVICE1='sell.py'
 
-if ps ax | grep -v grep | grep $SERVICE1 > /dev/null
-then
-    echo "$SERVICE1 service running "
-else
-    echo there is no such "$SERVICE1 service, starting"
-    python /usr/local/bin/sell.py
-fi
+ps -ef | grep $SERVICE1 | grep -v grep
+[ $?  -eq "0" ] && echo "$SERVICE1 process is running" || echo "$SERVICE1 process is not running, starting"; python /usr/local/bin/sell.py
+
 
 sleep 10
 done
