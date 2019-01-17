8aa83c7b592c4a8c1a22f85ac7c7498315281b9c
egaraev@gmail.com <Kax0vka44609>
2019-01-17 21:53:16 +0100

    changed start.sh

--- start.sh
+++ start.sh
@@ -8,17 +8,17 @@ do
 SERVICE0='buy.py'
 
 ps -ef | grep $SERVICE0 | grep -v grep
-[ $?  -eq "0" ] && echo "$SERVICE0 process is running" || echo "$SERVICE0 process is not running, starting"; python /usr/local/bin/buy.py
+[ $?  -eq "0" ] && echo "$SERVICE0 process is running" || echo "$SERVICE0 process is not running, starting"; python /usr/local/bin/buy.py &
 
 
-
+sleep 5
 
 
 SERVICE1='sell.py'
 
 ps -ef | grep $SERVICE1 | grep -v grep
-[ $?  -eq "0" ] && echo "$SERVICE1 process is running" || echo "$SERVICE1 process is not running, starting"; python /usr/local/bin/sell.py
+[ $?  -eq "0" ] && echo "$SERVICE1 process is running" || echo "$SERVICE1 process is not running, starting"; python /usr/local/bin/sell.py &
 
 
-sleep 10
+sleep 5
 done
