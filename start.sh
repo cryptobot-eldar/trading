c515082e4b50fffdd270cfb66edacd0259957ed5
egaraev@gmail.com <Kax0vka44609>
2019-01-17 22:17:23 +0100

    changes start.sh

--- start.sh
+++ start.sh
@@ -1,23 +1,6 @@
 #!/bin/bash
 #exec &>>/var/log/work.log
 
-while true
-do
+./start_buy.sh&
+./start_sell.sh&
 
-  for i in 1; do
-    SERVICE0='buy.py'
-    ps -ef | grep $SERVICE0 | grep -v grep
-    [ $?  -eq "0" ] && echo "$SERVICE0 process is running" || echo "$SERVICE0 process is not running, starting"; python /usr/local/bin/buy.py
-  done
-sleep 5
-
-
-
-  for i in 1; do
-    SERVICE1='sell.py'
-    ps -ef | grep $SERVICE1 | grep -v grep
-    [ $?  -eq "0" ] && echo "$SERVICE1 process is running" || echo "$SERVICE1 process is not running, starting"; python /usr/local/bin/sell.py
-  done
-
-sleep 5
-done
