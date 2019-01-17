f86ab5e686307e537eaf89b9e5ded530141b8b7c
egaraev@gmail.com <Kax0vka44609>
2019-01-17 22:03:36 +0100

    dcd

--- start.sh
+++ start.sh
@@ -4,21 +4,20 @@
 while true
 do
 
-
-SERVICE0='buy.py'
-
-ps -ef | grep $SERVICE0 | grep -v grep
-[ $?  -eq "0" ] && echo "$SERVICE0 process is running" || echo "$SERVICE0 process is not running, starting"; python /usr/local/bin/buy.py &
-
-
+  for i in 1; do
+    SERVICE0='buy.py'
+    ps -ef | grep $SERVICE0 | grep -v grep
+    [ $?  -eq "0" ] && echo "$SERVICE0 process is running" || echo "$SERVICE0 process is not running, starting"; python /usr/local/bin/buy.py
+  done
 sleep 5
 
 
-SERVICE1='sell.py'
 
-ps -ef | grep $SERVICE1 | grep -v grep
-[ $?  -eq "0" ] && echo "$SERVICE1 process is running" || echo "$SERVICE1 process is not running, starting"; python /usr/local/bin/sell.py &
-
+  for i in 1; do
+    SERVICE1='sell.py'
+    ps -ef | grep $SERVICE1 | grep -v grep
+    [ $?  -eq "0" ] && echo "$SERVICE1 process is running" || echo "$SERVICE1 process is not running, starting"; python /usr/local/bin/sell.py
+  done
 
 sleep 5
 done
