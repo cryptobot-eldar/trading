b2116d10359140a1f81b3d03b9b20fa8c4692090
egaraev@gmail.com <Kax0vka44609>
2019-01-22 22:21:04 +0100

    fxies

--- sell.py
+++ sell.py
@@ -187,9 +187,9 @@ def tick():
                 try:
                     db = MySQLdb.connect("database-service", "cryptouser", "123456", "cryptodb")
                     cursor = db.cursor()
-                    cursor.execute("SELECT GROUP_CONCAT(signals) FROM orderlogs where orderid=%s", (orderid))
-                    history = cursor.fetchone()[0]
-                    cursor.execute("update orders set history=%s where order_id=%s",(history, orderid))
+                    cursor.execute('SELECT GROUP_CONCAT(signals) FROM orderlogs where orderid=("%s")' % orderid)
+                    history = cursor.fetchone()
+                    cursor.execute('update orders set history=%s where order_id=%s',(history, orderid))
                     db.commit()
                 except MySQLdb.Error, e:
                     print "Error %d: %s" % (e.args[0], e.args[1])
